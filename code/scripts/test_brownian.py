6d5db828070bbee7a5c86a4b3a34b61842719ae0
Jonathan Nairn Tallen <jonathannairn@hotmail.com>
2024-01-29 17:06:16 +0100

    changes

--- code/scripts/test_brownian.py
+++ code/scripts/test_brownian.py
@@ -2,7 +2,6 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import pandas as pd
-from joblib import Parallel, delayed
 from generate_data.base_data import get_base_variables
 from equations.down_and_out_call_Brown import price_down_and_out_call_brown
 import time
@@ -10,7 +9,7 @@ import time
 def main(): 
     # Initial values
     h_values = range(99, 100)
-    n = 4 * 10**7
+    n = 2 * 10**7
     
     # Get base variables
     m, r, T, sigma, S0, K, trading_days, beta, H, q = get_base_variables()
